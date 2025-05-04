9057d24835ff83d45fba90ec909cde490c451326
lyuda140707 <lyuda14070702@icloud.com>
2025-05-04 16:34:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,7 @@ async def payment_check(request: Request):
         str(data.get("amount", "")),
         data.get("currency", ""),
         data.get("authCode", ""),
-        data.get("cardPan", "")
+        data.get("cardPan", ""),
+        data.get("transactionStatus", ""),
+        data.get("reasonCode", "")
+    ]
